d71cdfe66bb0d75b40d680c2db1eb2aea8ea07ff
Sachin Lovanshi <sachinlovanshi13@gmail.com>
2026-02-11 21:46:46 +0530

    Nqueens

---
+++ Problem1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std; 
+
+class Solution {
+public:
+bool isSafe(int row,int col,vector<string> &board,int n){
+    int i=row;
+    int j=col;
+    while(i>=0 && j>=0){
+      if(board[i][j]=='Q'){return false;}
+      i--;
+      j--;
+    }
+    i=row;
+    j=col;
+        while(i<n&&j>=0){
+      if(board[i][j]=='Q'){return false;}
+      i++;
+      j--;
+    }
+    i=row;
+    j=col;
+    while(j>=0){
+        if(board[i][j]=='Q'){return false;}
+        j--;
+    }
+    return true;
+}
+void solve(int col,vector<string> &board,vector<vector<string>>&ans,int n){
+    if(col==n){
+        ans.push_back(board);
+        return;
+    }
+    for(int row=0;row<n;row++){
+        if(isSafe(row,col,board,n)){
+            board[row][col]='Q';
+            solve(col+1,board,ans,n);
+            board[row][col]='.';
+        }
+    }
+}
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string> board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        solve(0,board,ans,n);
+        return ans;
+    }
+};
