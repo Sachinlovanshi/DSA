4c0afa53ed4d766a8b709512c38542bb44a7f831
Sachin Lovanshi <sachinlovanshi13@gmail.com>
2026-02-27 22:58:23 +0530

    codeforces contes

---
+++ Problem13.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+       vector<int> v(n);
+       for(int i=0;i<n;i++){
+           cin >> v[i];
+       }
+       int maxi=v[0];
+       int count=1;
+         for(int i=1;i<n;i++){
+              if(v[i]>maxi){
+                maxi=v[i];
+                count=1;
+              }
+              else if(v[i]==maxi){
+                count++;
+              }
+         }
+         cout << count << endl;
+    }
+}
